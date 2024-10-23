7f748c24b77716f3e43ffb8d5bf437b351010477
Dukyoon Im <DUKY8N@gmail.com>
2024-10-23 11:17:37 +0900

    feat: Button 컴포넌트 생성

---
+++ app/components/Button.tsx
@@ -0,0 +1,25 @@
+import style from './Button.module.css';
+
+type ButtonProps = {
+  children: React.ReactNode;
+  color?: 'primary' | 'success' | 'info' | 'warning' | 'important';
+  href?: string;
+  onClick?: () => void;
+};
+const Button = ({ children, color = 'primary', href, onClick }: ButtonProps) => {
+  if (href) {
+    return (
+      <a className={`${style.button} ${style[color]}`} href={href} onClick={onClick}>
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <button className={`${style.button} ${style[color]}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
