cde1e2247f2f7f728585774d63b62a2d8b1fee8a
Dukyoon Im <DUKY8N@gmail.com>
2024-10-24 18:39:38 +0900

    feat: 로고 아이콘 이미지 위에 설명을 표시하는 효과 추가

--- app/Logo.tsx
+++ app/Logo.tsx
@@ -7,6 +7,7 @@ import styles from './page.module.css';
 const Logo = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const imageRef = useRef<HTMLImageElement | null>(null);
+  const spanRef = useRef<HTMLSpanElement | null>(null);
 
   const otherImages = [
     '/example-1.jpg',
@@ -16,46 +17,57 @@ const Logo = () => {
     '/example-5.jpg',
   ];
 
+  const otherImagesDescription = ['Webcam', 'Presentations', 'Code', 'Idea', 'Gaming skills'];
+
   useEffect(() => {
     const interval = setInterval(() => {
-      if (imageRef.current) {
-        // 이미지 크기 줄이기
-        imageRef.current.style.transition = 'transform 0.3s ease-in-out';
-        imageRef.current.style.transform = 'scale(0.8)';
+      // CSS 애니메이션 효과
+      if (imageRef.current) imageRef.current.style.transform = 'scale(0.8)';
+      if (spanRef.current) spanRef.current.style.transform = 'translate(-50%, -10%)';
+      if (spanRef.current) spanRef.current.style.opacity = '0';
 
-        // 300ms 후 이미지 원래 크기로 되돌리고, 이미지 변경
-        setTimeout(() => {
-          if (imageRef.current) {
-            // 원래 크기로
-            imageRef.current.style.transform = 'scale(1)';
-          }
+      // 300ms 후 원래 상태로 되돌리기
+      setTimeout(() => {
+        if (imageRef.current) imageRef.current.style.transform = 'scale(1)';
+        if (spanRef.current) spanRef.current.style.transform = 'translate(-50%, -50%)';
 
-          // 이미지 인덱스 증가
-          setCurrentImageIndex((prevIndex) => (prevIndex + 1) % (otherImages.length * 2));
-        }, 300); // 애니메이션 시간만큼 딜레이
-      }
+        // 이미지 인덱스 증가
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % (otherImages.length * 2));
+      }, 300);
     }, 3000);
 
     return () => clearInterval(interval); // 컴포넌트 언마운트 시 정리
   }, [otherImages.length]);
 
+  useEffect(() => {
+    if (currentImageIndex % 2 !== 0) {
+      if (spanRef.current) spanRef.current.style.opacity = '1';
+    }
+  }, [currentImageIndex]);
+
   const currentImage =
     currentImageIndex % 2 === 0 ? '/logo.png' : otherImages[Math.floor(currentImageIndex / 2)];
+
+  const currentImageDescription =
+    currentImageIndex % 2 === 0 ? '' : otherImagesDescription[Math.floor(currentImageIndex / 2)];
 
   const imageStyle = currentImage === '/logo.png' ? { border: 'none' } : {};
 
   return (
     <div className={styles.logo}>
       <h1>Show My</h1>
-      <Image
-        ref={imageRef}
-        src={currentImage}
-        alt="logo icon"
-        width={150}
-        height={150}
-        quality={100}
-        style={imageStyle}
-      />
+      <div className={styles['logo-icon-wrapper']}>
+        <Image
+          ref={imageRef}
+          src={currentImage}
+          alt="logo icon"
+          width={150}
+          height={150}
+          quality={100}
+          style={imageStyle}
+        />
+        <span ref={spanRef}>{currentImageDescription}</span>
+      </div>
     </div>
   );
 };
