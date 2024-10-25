48af05cde8bb54e1ed16edbf9115804cc9ef55b3
Dukyoon Im <DUKY8N@gmail.com>
2024-10-25 23:27:15 +0900

    feat: Input 컴포넌트 구현

---
+++ components/Input.tsx
@@ -0,0 +1,17 @@
+import style from './Input.module.css';
+
+type InputProps = {
+  label?: string;
+  placeholder?: string;
+  ref?: React.RefObject<HTMLInputElement>;
+};
+const Input = ({ label, placeholder, ref }: InputProps) => {
+  return (
+    <div className={style['input-container']}>
+      {label ? <label htmlFor="nickname">{label}</label> : null}
+      <input id="nickname" type="text" placeholder={placeholder} ref={ref} />
+    </div>
+  );
+};
+
+export default Input;
