6a2edc588976e6f05f49f3d675252d54503a3686
Dukyoon Im <DUKY8N@gmail.com>
2024-11-04 00:16:10 +0900

    refactor: ThumbnailVideo 컴포넌트에 forwardRef와 props 확장 기능 추가하여 유연성 향상

--- components/ThumbnailVideo.tsx
+++ components/ThumbnailVideo.tsx
@@ -1,31 +1,37 @@
+import React from 'react';
 import Image from 'next/image';
 import styles from './ThumbnailVideo.module.css';
 
 type ThumbnailVideoProps = {
   isFocus?: boolean;
   isScreenSharing?: boolean;
-};
-const ThumbnailVideo = ({ isFocus = false, isScreenSharing = false }: ThumbnailVideoProps) => {
-  return (
-    <div
-      className={`
-        ${styles['thumbnail-video']}
-        ${isFocus ? styles.focus : ''}
-        ${isScreenSharing ? styles['screen-sharing'] : ''}
-      `}
-    >
-      <video />
-      <div className={styles.nickname}>
-        <Image
-          src={isScreenSharing ? '/screen-share-white.svg' : '/person-white.svg'}
-          alt={'icon'}
-          width={12}
-          height={12}
-        />
-        <span>테스트</span>
+} & React.ComponentPropsWithRef<'video'>;
+
+const ThumbnailVideo = React.forwardRef<HTMLVideoElement, ThumbnailVideoProps>(
+  ({ isFocus = false, isScreenSharing = false, ...rest }, ref) => {
+    return (
+      <div
+        className={`
+          ${styles['thumbnail-video']}
+          ${isFocus ? styles.focus : ''}
+          ${isScreenSharing ? styles['screen-sharing'] : ''}
+        `}
+      >
+        <video ref={ref} {...rest} />
+        <div className={styles.nickname}>
+          <Image
+            src={isScreenSharing ? '/screen-share-white.svg' : '/person-white.svg'}
+            alt={'icon'}
+            width={12}
+            height={12}
+          />
+          <span>테스트</span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  },
+);
+
+ThumbnailVideo.displayName = 'ThumbnailVideo';
 
 export default ThumbnailVideo;
